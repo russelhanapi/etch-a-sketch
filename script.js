de4a01426db9f87ca16ce3d02ede5aa9908c57e7
Russel Amin Kadra P. Hanapi <92841378+russelhanapi@users.noreply.github.com>
2025-01-24 12:53:58 +0800

    Add button for grid size customization
    - Added a button (btnSetNumOfPixels) to allow users to specify the number of squares per side.
    - Created a setNumOfSquare function to prompt for input and regenerate the grid.
    - Cleared the existing grid before generating a new one.
    - Validated user input to ensure a positive number not exceeding 100.
    - Added style to btnSetNumOfPixels
    - Organized the code for better structure and improved readability.

--- script.js
+++ script.js
@@ -12,11 +12,13 @@ const generatePixels = function (n) {
   }
 };
 
-const outerContainer = document.createElement('div');
-outerContainer.classList.add('outer-container');
-document.body.appendChild(outerContainer);
-
-generatePixels(16);
+const setNumOfSquare = function () {
+  let n = Number(prompt('Enter number of pixels'));
+  while (isNaN(n) || n < 1 || n >= 100)
+    n = Number(prompt('Input must be a positive number NOT greater than 100'));
+  outerContainer.innerHTML = '';
+  generatePixels(n);
+};
 
 const draw = function (e) {
   if (e.target.classList.contains('pixel')) {
@@ -24,4 +26,18 @@ const draw = function (e) {
   }
 };
 
+// ****************************************8
+
+const btnSetNumOfPixels = document.createElement('button');
+btnSetNumOfPixels.textContent = 'Set Number of Square';
+btnSetNumOfPixels.classList.add('btn-set-num-of-pixels');
+document.body.appendChild(btnSetNumOfPixels);
+
+const outerContainer = document.createElement('div');
+outerContainer.classList.add('outer-container');
+document.body.appendChild(outerContainer);
+
+generatePixels(16);
+
+btnSetNumOfPixels.addEventListener('click', setNumOfSquare);
 outerContainer.addEventListener('mouseover', draw);
